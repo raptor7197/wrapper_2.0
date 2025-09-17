06a3e6c3eb491297f3ea8f4e8da26b27f65ab8a5
raptor7197 <vamsikrishna75836@gmail.com>
2025-09-17 12:43:12 +0530

    CORS error fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173','https://wrapper-2-0.vercel.app/'],
   methods: ['GET', 'POST'],
   credentials: true
 }));
